4283c436c8f35d3c612ed9ea42e0413c3d1d1914
techacct <ademolaoladapo@gmail.com>
2025-07-08 04:52:48 +0100

    loaded data into raw layer using seeds because the file size are under 10MB'

--- macros/generate_custom_schema.sql
+++ macros/generate_custom_schema.sql
@@ -1,14 +1,10 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-
-    {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
-
+    {% set default_schema = target.schema %}
+    {% set custom_schema = custom_schema_name or node.config.schema %}
+    
+    {% if custom_schema is none %}
         {{ default_schema }}
-
-    {%- else -%}
-
-        {{ custom_schema_name | trim }}
-
-    {%- endif -%}
-
+    {% else %}
+        {{ custom_schema | trim }}
+    {% endif %}
 {%- endmacro %}
